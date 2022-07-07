63e6287ee5397344a8b39e633adcd654755fca87
BillyVII <gabrielasteria@gmail.com>
2022-07-07 10:12:11 +0200

    part 1 terminé, mise en place storyboard

--- EViOS1/EViOS1/ViewController.swift
+++ EViOS1/EViOS1/ViewController.swift
@@ -8,17 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet var userImage: UIImage!
-    @IBOutlet var label: UILabel!
-    @IBOutlet var loginField: UITextField!
-    @IBOutlet var pwdField: UITextField!
+    @IBOutlet weak var userImage: UIImageView!
     
+    @IBOutlet weak var loginField: UITextField!
     
+    @IBOutlet weak var pwdField: UITextField!
+    
+    @IBOutlet weak var swift: UISwitch!
+    
+    @IBOutlet weak var button: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+      
     }
 
-
+    override func viewDidAppear(_ animated: Bool) {
+        userImage.layer.cornerRadius = userImage.frame.width / 2
+    }
 }
 
